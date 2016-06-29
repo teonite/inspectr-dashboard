1ca46cf44a7c23f7706e0cb73fb63b6b3896456e
Jacek Chmielewski <jchmielewski@teonite.com>
2016-06-29 23:40:30 +0200

    sort projects by rank

--- app/containers/DashboardPage/index.js
+++ app/containers/DashboardPage/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { subscribe } from 'horizon-react';
 import ReportBlock from 'containers/ReportBlock';
-
+import { rankProject } from 'utils/ranking';
 
 class DashboardPage extends React.Component { // eslint-disable-line react/prefer-stateless-function
+
+  projectsSortedByRank() {
+    return this.props.projects.sort((p1, p2) => rankProject(p2) - rankProject(p1))
+  }
 
   render() {
     return (
       <div className="row">
-        {this.props.projects.map((project, index) => <ReportBlock key={index} project={project}/>)}
+        {this.projectsSortedByRank().map((project, index) => <ReportBlock key={index} project={project}/>)}
       </div>
     );
   }
