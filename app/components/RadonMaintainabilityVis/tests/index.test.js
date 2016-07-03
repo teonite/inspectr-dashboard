1aefc7543b24a41130c95681502c19eb51ef6c42
Jacek Chmielewski <jchmielewski@teonite.com>
2016-07-03 10:17:31 +0200

    eslint fixes

--- app/components/RadonMaintainabilityVis/tests/index.test.js
+++ app/components/RadonMaintainabilityVis/tests/index.test.js
@@ -1,9 +1,3 @@
-import RadonMaintainabilityVis from '../index';
-
-import expect from 'expect';
-import { shallow } from 'enzyme';
-import React from 'react';
-
 describe('<RadonMaintainabilityVis />', () => {
 
 });
