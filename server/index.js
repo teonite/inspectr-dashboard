c42d71f0cbdbf7a2d9163899f9516fb85be3ed73
Jacek Chmielewski <jchmielewski@teonite.com>
2016-09-21 09:13:21 +0200

    upgraded horizon server and client libs

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 /* eslint consistent-return:0 */
 
 const express = require('express');
+const proxy = require('http-proxy-middleware');
 const logger = require('./logger');
 
 const argv = require('minimist')(process.argv.slice(2));
@@ -9,14 +10,15 @@ const isDev = process.env.NODE_ENV !== 'production';
 const ngrok = (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel ? require('ngrok') : false;
 const resolve = require('path').resolve;
 
-// If you need a backend, e.g. an API, add your custom backend-specific middleware here
-// app.use('/api', myApi);
-
 // In production we need to pass these values in instead of relying on webpack
 const app = setup(express(), {
   outputPath: resolve(process.cwd(), 'build'),
   publicPath: '/',
 });
+
+// If you need a backend, e.g. an API, add your custom backend-specific middleware here
+// app.use('/api', myApi);
+app.use('/horizon', proxy({target: 'http://localhost:8181', changeOrigin: true}));
 
 // get the intended port number, use port 3000 if not provided
 const port = argv.port || process.env.PORT || 3000;
