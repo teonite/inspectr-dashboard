2c1e866b9c280376336638ba50dd264e330a497d
VanDavv <lukpila29@gmail.com>
2016-09-30 13:58:54 +0200

    [INSPECTR-5] History prop is ready to be injected to history component]

--- app/containers/ProjectDetailsPage/index.js
+++ app/containers/ProjectDetailsPage/index.js
@@ -7,17 +7,6 @@ import ProjectDetails from 'components/ProjectDetails';
 class ProjectDetailsPage extends React.Component { // eslint-disable-line react/prefer-stateless-function
 
   render() {
-    /*
-      I didn't find a way to do it with horizon. Query in ReQL that does the job:
-      r.db('inspectr').table("reports").innerJoin(r.db('inspectr').table("reports_history"), function(reportRow, historyRow) {
-      	return reportRow('id').eq([PROJECT_ID])
-      }).zip()
-    */
-    let history = []
-    this.props.history.forEach((entry) => {
-      if(entry.project_name == this.props.projects[0].project_name) history.push(entry)
-    })
-
     return (
       <div>
         {this.props.projects.map((project, index) => {
@@ -35,7 +24,7 @@ ProjectDetailsPage.propTypes = {
 
 const mapDataToProps = {
   projects: (hz, props) => hz('reports').find(props.params.projectId),
-  history: (hz, props) => hz('reports_history')
+  history: (hz, props) => hz('reports_history').findAll({'project_id' : props.params.projectId})
 };
 
 export default subscribe({
