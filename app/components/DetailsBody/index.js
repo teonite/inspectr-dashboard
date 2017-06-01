7d90a5c2a793e39717daf0000de9dce09ac85529
jmarciniak <uitoteonite31337>
2017-06-01 15:19:47 +0200

    dodanie dropdowna usuwania projektu i poprawki w widoku szczegółów

--- app/components/DetailsBody/index.js
+++ app/components/DetailsBody/index.js
@@ -9,24 +9,36 @@ function DetailsBody({project}) {
   const fullRank = `${projectRank} / ${MAX_RANK}`;
 
   return (
-      <div className="row">
-        <div className="col-xs-12">
-          <div className="project-box details">
-            <div className="header row">
-              <span title={fullRank} className={rankClassName}/>
-              <div className="col-md-8">
-                <h2>{project.project_name}</h2>
-                <p className="subtext">Rank: {projectRank} <span className="text-dark-gray">/</span> Last update: {formatDate(project.time_created)}</p>
-              </div>
-              <div className="col-md-4 mobile-off">
-                <div className="stats-box">
-                  <h3>Number of tests</h3>
-                  <span className="circle">{project.reports.length}</span>
-                  <span className="line" />
+      <div className="dashboard-wrapper">
+        <div className="row">
+          <div className="col-xs-12">
+            <div className="project-box details">
+              <div className="header row">
+                <span title={fullRank} className={rankClassName}/>
+                <div className="col-sm-6 col-xs-12">
+                  <h2>{project.project_name}</h2>
+                  <p className="subtext">Rank: {projectRank} <span className="text-dark-gray">/</span> Last update: {formatDate(project.time_created)}</p>
+                </div>
+                <div className="col-sm-6 col-xs-12">
+                  <div className="options-box dropdown">
+                    <button className="btn dropdown-toggle circle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                      <i className="glyphicon glyphicon-cog" />
+                    </button>
+                    <ul className="dropdown-menu" aria-labelledby="ProjectOptionsMenu">
+                      <li>
+                        <a href="#"><i className="glyphicon glyphicon-trash" />Delete project</a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="stats-box mobile-off">
+                    <p>Number of tests</p>
+                    <span className="circle">{project.reports.length}</span>
+                    <span className="line" />
+                  </div>
                 </div>
               </div>
+              {project.reports.map((report, index) => <Report report={report} key={index}/>)}
             </div>
-            {project.reports.map((report, index) => <Report report={report} key={index}/>)}
           </div>
         </div>
       </div>
